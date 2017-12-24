a57848fc838615d2919b3ef2a214b78437146f5e
王梦 <wangmeng@sinomoses.com>
2017-12-24 23:22:39 +0800

    添加订单表、订单详情表表sql并更新文档

--- doc/iLife.sql
+++ doc/iLife.sql
@@ -43,3 +43,45 @@ CREATE TABLE `i_goods_cart_info` (
   PRIMARY KEY (`id`),
   KEY `idx_name` (`name`)
 ) ENGINE=MyISAM AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for i_order_info
+-- ----------------------------
+DROP TABLE IF EXISTS `i_order_info`;
+CREATE TABLE `i_order_info` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `order_no` varchar(50) NOT NULL COMMENT '订单号',
+  `total_price` decimal(10,2) NOT NULL COMMENT '订单金额',
+  `total_num` int(10) unsigned DEFAULT 0 COMMENT '商品总数',
+  `oper_time` datetime NOT NULL COMMENT '下单时间',
+  `uid` int(10) unsigned NOT NULL COMMENT '下单人id',
+  `username` varchar(30) NOT NULL COMMENT '下单人',
+  `is_valid` tinyint(1) unsigned NOT NULL COMMENT '是否有效',
+  `cancle_flag` tinyint(1) unsigned NOT NULL COMMENT '是否取消',
+  PRIMARY KEY (`id`),
+  KEY `idx_name` (`name`)
+) ENGINE=MyISAM AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for i_order_goods_info
+-- ----------------------------
+DROP TABLE IF EXISTS `i_order_goods_info`;
+CREATE TABLE `i_order_goods_info` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `order_id` int(10) unsigned NOT NULL COMMENT '订单id',
+  `order_no` varchar(50) NOT NULL COMMENT '订单号',
+  `goods_id` int(10) unsigned NOT NULL COMMENT '商品id',
+  `goods_t_id` int(10) unsigned NOT NULL COMMENT '商品t_id',
+  `goods_name` varchar(20) NOT NULL COMMENT '商品名称',
+  `goods_num` int(10) unsigned DEFAULT '0' COMMENT '商品数量',
+  `goods_unit` varchar(10) NOT NULL COMMENT '商品单位',
+  `goods_price` decimal(10,2) NOT NULL COMMENT '商品单价',
+  `total_price` decimal(10,2) NOT NULL COMMENT '商品总价',
+  `oper_time` datetime NOT NULL COMMENT '下单时间',
+  `uid` int(10) unsigned NOT NULL COMMENT '下单人id',
+  `username` varchar(30) NOT NULL COMMENT '下单人',
+  `is_valid` tinyint(1) unsigned NOT NULL COMMENT '是否有效',
+  `cancle_flag` tinyint(1) unsigned NOT NULL COMMENT '是否取消',
+  PRIMARY KEY (`id`),
+  KEY `idx_name` (`name`)
+) ENGINE=MyISAM AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
